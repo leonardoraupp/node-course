5af553fbe9bab9f32529df5c3b8ab07f90d9119d
ctllraupp <ctl_lraupp@uolinc.com>
2023-06-16 17:57:32 -0300

    Comportamento do this no node dentro de uma arrow function

--- this.js
+++ this.js
@@ -1,15 +1,30 @@
-// comportamento do this em diferentes escopos
+console.log("Comportamento do this em diferentes escopos")
+console.log()
 
-console.log(this === global)
-console.log(this === module.exports)
-console.log(this === exports)
+console.log("No módulo: ")
+console.log("Não aponta para o global -> ", this === global)
+console.log("Aponta para module.exports -> ", this === module.exports)
+console.log("Aponta para exports -> ", this === exports)
 
-function thisFunc() {
-    console.log("Dentro da função...")
-    console.log(this === global)    
-    console.log(this === module.exports)
-    console.log(this === exports)
+console.log()
+console.log()
+
+function thisFunctionExpression() {
+    console.log("Dentro da função de expressão:")
+    console.log("Aponta para o global  -> ", this === global)    
+    console.log("Não aponta para module.exports -> ", this === module.exports)
+    console.log("Não aponta para exports -> ", this === exports)
 }
+const thisArrowFunction = () => {
+    console.log("Dentro da arrow function: (Se comporta como se estivesse no módulo novamente)")
+    console.log("Não aponta para o global -> ", this === global)
+    console.log("Aponta para module.exports -> ", this === module.exports)
+    console.log("Aponta para exports -> ", this === exports)    
+    console.log()
+    }
 
-thisFunc()
+thisFunctionExpression()
+console.log()
+console.log()
+thisArrowFunction()
 
